e39e8410b4d27acf2e2a9ca5c1c660b5389a489e
elvisuriel <urielmarciales@gmil.com>
2024-06-11 07:28:32 -0500

    ajustes en el de ventas

--- src/components/ventas/Ventas.tsx
+++ src/components/ventas/Ventas.tsx
@@ -5,7 +5,7 @@ import { RiLineChartLine } from 'react-icons/ri';
 const Ventas: React.FC = () => {
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 mt-10 gap-8">
-            <div className="bg-primary-100 p-8 rounded-xl text-gray-300 flex flex-col gap-6 xl:col-span-2">
+            <div className=" p-8 rounded-xl text-gray-300 flex flex-col gap-6 xl:col-span-2 " style={{ backgroundColor: "#004467" }}>
                 <RiLineChartLine className="text-5xl" />
                 <h4 className="text-2xl">Abril</h4>
                 <span className="text-5xl text-white">$ 250,000</span>
@@ -16,7 +16,7 @@ const Ventas: React.FC = () => {
                     <EarningsChart />
                 </div>
             </div>
-            <div className="xl:col-span-3 bg-primary-100 p-8 rounded-xl text-gray-300 flex flex-col gap-6">
+            <div className="xl:col-span-3 bg-primary-100 p-8 rounded-xl text-gray-300 flex flex-col gap-6" style={{ backgroundImage: "url('https://res.cloudinary.com/dybws2ubw/image/upload/v1718108592/ventas_n1xvug.jpg') " }}>
                 <div className="p-4">
                     <h1 className="text-3xl mb-6">Ventas</h1>
                 </div>
